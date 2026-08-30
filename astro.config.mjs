3570201ee62ccf60af9e114f13b6c4ed71db76cc
rhinotecbekasi <chandrajoyko@gmail.com>
2026-08-30 17:21:14 +0700

    chore: sync astro.config.mjs build config for wrangler deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  build: {
-    assets: 'assets'
-  },
   compressHTML: true,
   adapter: cloudflare({
     imageService: 'passthrough',
@@ -21,16 +18,5 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false
     })
-  ],
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: 'assets/rancangloka-app-[hash].js',
-          chunkFileNames: 'assets/rancangloka-core-[hash].js',
-          assetFileNames: 'assets/rancangloka-[name]-[hash].[ext]'
-        }
-      }
-    }
-  }
+  ]
 });
